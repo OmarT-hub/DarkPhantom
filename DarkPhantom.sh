b46f16c8f5d0b42d09610c0cf776af07b71486a4
kali <xocxx897@gmail.com>
2025-04-08 20:23:15 +0200

    added DarkPhantom.sh

---
+++ DarkPhantom.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "                     ==================================="
+echo "                     Welcome to DarkPhantom bash script!"
+echo "                     ==================================="
+echo
+echo
+echo "(1) Run Scan_Networks_To_csv"
+echo "(2) Run Catch_Handshake_File"
+
+read -p "Enter your choice: " choice
+
+if [ "$choice" == "1" ]; then
+    echo "Running scan_networks_to_csv"
+    bash NETWORKS.sh
+elif [ "$choice" == "2" ]; then
+    echo "Running Catch_Handshake_File"
+   sudo bash catch_HS.sh
+else
+    echo "Invalid choice. Please run the script again and choose 1 or 2."
+fi
